3bff7f186037b225c17ca3a6f4cab39c87f84f08
Dylan Farrell <57410669+Swoopi@users.noreply.github.com>
2024-06-17 09:35:14 -0400

    setting & profile buttons redirect you

--- app/src/main/java/com/iptv/iptv2/activities/ProfileActivity.java
+++ app/src/main/java/com/iptv/iptv2/activities/ProfileActivity.java
@@ -10,6 +10,6 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_profile);
     }
 }
